a3716006f51c2a0fb7a6564b01926e3584af196d
Mik0 <210298@edu.p.lodz.pl>
2018-10-19 10:44:30 +0200

    Deleted unnecessary usings form Serialize.cs

--- Projekt.Model/Serialize.cs
+++ Projekt.Model/Serialize.cs
@@ -1,14 +1,7 @@
 ﻿using log4net;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
-using System.Xml.Serialization;
 
 namespace Projekt.Model
 {
