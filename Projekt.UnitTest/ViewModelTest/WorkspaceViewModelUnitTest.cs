19fe384cb218c74737e91306c26e5db993abdbed
Pan Architekt <whitepixel@protonmail.com>
2018-10-21 20:38:37 +0200

    Added test methods

--- Projekt.UnitTest/ViewModelTest/WorkspaceViewModelUnitTest.cs
+++ Projekt.UnitTest/ViewModelTest/WorkspaceViewModelUnitTest.cs
@@ -33,5 +33,31 @@ namespace Projekt.UnitTest.ViewModelTest
             WorkspaceViewModel workspaceViewModel = new WorkspaceViewModel();
             Assert.IsTrue(workspaceViewModel.ReadDataCommand.CanExecute(null));
         }
+        [TestMethod]
+        public void TestReadFileName()
+        {
+            WorkspaceViewModel workspaceViewModel = new WorkspaceViewModel();
+            Assert.AreEqual<String>(workspaceViewModel.ReadFileName, "Nie wybrano pliku");
+        }
+        [TestMethod]
+        public void TestReadFileNameChange()
+        {
+            WorkspaceViewModel workspaceViewModel = new WorkspaceViewModel();
+            workspaceViewModel.ReadFileName = "C:\\model.dll";
+            Assert.AreEqual<String>(workspaceViewModel.ReadFileName, "C:\\model.dll");
+        }
+        [TestMethod]
+        public void TestSaveFileName()
+        {
+            WorkspaceViewModel workspaceViewModel = new WorkspaceViewModel();
+            Assert.AreEqual<String>(workspaceViewModel.SaveFileName, "Nie wybrano pliku");
+        }
+        [TestMethod]
+        public void TestSaveFileNameChange()
+        {
+            WorkspaceViewModel workspaceViewModel = new WorkspaceViewModel();
+            workspaceViewModel.SaveFileName = "C:\\plik";
+            Assert.AreEqual<String>(workspaceViewModel.SaveFileName, "C:\\plik");
+        }
     }
 }
