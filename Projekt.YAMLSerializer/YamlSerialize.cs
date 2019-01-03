79a7b242d7b6f19bbf86d5525ca65d319480f0ff
Mik0 <210298@edu.p.lodz.pl>
2019-01-03 19:36:30 +0100

    Working on Yaml Serialization

--- Projekt.YAMLSerializer/YamlSerialize.cs
+++ Projekt.YAMLSerializer/YamlSerialize.cs
@@ -1,22 +1,32 @@
 ﻿using Projekt.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using YamlDotNet.Serialization;
 
 namespace Projekt.YAMLSerializer
 {
+    [PartCreationPolicy(CreationPolicy.NonShared)]
     public class YamlSerialize : IDataRepositoryService
     {
         public AssemblyModel Read(string path)
         {
             throw new NotImplementedException();
+
         }
 
         public void Save(AssemblyModel _object, string path)
         {
-            throw new NotImplementedException();
+            var serializer = new Serializer();
+
+            using (TextWriter writer = File.CreateText(path))
+            {
+                serializer.Serialize(writer, _object);
+            }            
         }
     }
 }
--- Projekt_Ver_2_0/App.xaml.cs
+++ Projekt_Ver_2_0/App.xaml.cs
@@ -20,6 +20,7 @@ namespace Projekt_Ver_2_0
             Compose.Instance.AddLocalAssemblyToCatalog(Projekt_Ver_2_0.Properties.Settings.Default.DatabaseService);
             Compose.Instance.AddLocalAssemblyToCatalog(Projekt_Ver_2_0.Properties.Settings.Default.DatabaseModel);
             Compose.Instance.AddLocalAssemblyToCatalog(Projekt_Ver_2_0.Properties.Settings.Default.LoggerService);
+            Compose compose = Compose.Instance;
             Compose.Instance.Container.ComposeExportedValue<IOpenFilePathService>(new GraphicalOpenFilePathService());
             Compose.Instance.Container.ComposeExportedValue<ISaveFilePathService>(new GraphicalSaveFilePathService());
         }
